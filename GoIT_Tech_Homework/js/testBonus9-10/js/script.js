b8bc4296b7f6c8c99ab65fb8e526e1b0644d63fe
urchenko <urchenkomaksim@gmail.com>
2016-04-19 21:09:32 +0300

    testBonus9-10

---
+++ GoIT_Tech_Homework/js/testBonus9-10/js/script.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  var element = document.querySelectorAll('.dropdown');
+
+  for(var i = 0; i < element.length; i++) {
+    element[i].addEventListener('mouseover', function () {
+      debugger;
+      var
+     console.log(this.querySelector('.submenu'));
+     this.querySelector('.submenu').style.display = 'block';
+     // if (this.style.opacity == 0) { 
+     //   slideDown(this, 1000);
+     // } 
+    });
+    element[i].addEventListener('mouseout', function () {
+     // console.log(this);
+     this.querySelector('.submenu').style.display ='none';
+     // slideUp();
+    });
+  }
+
+function slideDown(el, duration) {
+  // debugger;
+  var startOpacity = el.style.opacity;
+  var time = 0;
+  var fps = 25;
+
+  var interval = setInterval( function () {
+    
+    if (time >= duration) {
+      clearInterval(interval);
+    }
+
+    startOpacity += 1/(duration/fps);
+    time += duration/fps;
+    el.style.opacity = startOpacity;
+  }, duration/fps);
+}
+}
